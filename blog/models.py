e3a3479b65cfc8e14cfe3222f55f1dd309af3ca0
Hood Adrian <fact.serv.casa@gmail.com>
2023-11-06 18:21:21 -0300

    creacion de los modelos

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    nombre = models.CharField (max_length=64)
+    apellido = models.CharField (max_length=64)
+    email = models.EmailField (blank= False)
+    fecha_nac = models.DateField(null=True)
+    dni = models.CharField (max_length=20)
+    telefono = models.CharField (max_length=30, blank= True)
+    
+class Temas (models.Model):
+    title = models.CharField (max_length=64)
+    creador = models.CharField (max_length=64)
+
+class post (models.Model):
+    title = models.CharField (max_length=64)
+    post = models.CharField (max_length=256)
+    signature = models.CharField (max_length=64)
